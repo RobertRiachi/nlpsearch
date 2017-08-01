a3bef0e5bd5bb0518dbab03d43293d7d4cdefb62
Robert Riachi <robertjr@riachi.com>
2017-08-01 15:32:15 -0400

    Fixed Bug p.2

--- Graph.py
+++ Graph.py
@@ -6,7 +6,7 @@ neograph = Graph(password="password")
 
 #Wordpath: takes a word and a neo4j graph database, returns the cat nodes beloging to shortest path to the root node
 def wordpath(word, ngraph):
-	path = ngraph.data("MATCH path = shortestpath((x:Category {catName: "word"})-[:SUBCAT_OF*]->(root:RootCategory)) RETURN path")
+	path = ngraph.data("MATCH path = shortestpath((x:Category {catName: "+word+"})-[:SUBCAT_OF*]->(root:RootCategory)) RETURN path")
 	words = re.findall('"([^"]*)"', str(path))
 	return words
 
